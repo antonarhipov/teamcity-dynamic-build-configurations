462ee05c93b4831406e1c39aeb7c1c95b54cd875
Anton Arhipov <ant.arhipov@gmail.com>
2019-03-01 00:53:12 +0200

    going crazy

--- .teamcity/settings.kts
+++ .teamcity/settings.kts
@@ -1,7 +1,6 @@
 import jetbrains.buildServer.configs.kotlin.v10.toExtId
 import jetbrains.buildServer.configs.kotlin.v2018_2.*
 import jetbrains.buildServer.configs.kotlin.v2018_2.buildSteps.maven
-import jetbrains.buildServer.configs.kotlin.v2018_2.buildSteps.script
 
 version = "2018.2"
 
@@ -9,11 +8,15 @@ val operatingSystems = listOf("Mac OS X", "Windows", "Linux")
 val jdkVersions = listOf("JDK_18", "JDK_11")
 
 project {
-    for (os in operatingSystems) {
-        for (jdk in jdkVersions) {
-            buildType(Build(os, jdk))
-        }
-    }
+//    for (os in operatingSystems) {
+//        for (jdk in jdkVersions) {
+//            buildType(Build(os, jdk))
+//        }
+//    }
+
+    forEachJVMAndOS {os, jdk ->
+        build(os, jdk)
+    }.forEach(this::buildType)
 }
 
 class Build(val os: String, val jdk: String) : BuildType({
@@ -23,19 +26,95 @@ class Build(val os: String, val jdk: String) : BuildType({
     vcs {
         root(DslContext.settingsRoot)
     }
+})
+
+
+fun forEachJVMAndOS(a : (BuildOS, JdkVersion) -> BuildType?) : List<BuildType> {
+    return JdkVersion.values().flatMap { jdk ->
+        BuildOS.values().mapNotNull { os ->
+            a(os, jdk)
+        }
+    }
+}
+
+open class JdkAndOsBuildType(
+    val os: BuildOS,
+    val jdk: JdkVersion
+) : BuildType({
+
+    vcs {
+        root(DslContext.settingsRoot)
+    }
 
     steps {
-//        maven {
-//            goals = "clean package"
-//            mavenVersion = defaultProvidedVersion()
-//            jdkHome = "%env.${jdk}%"
-//        }
-        script {
-            scriptContent = "echo $os, $jdk"
+        maven {
+            goals = "clean package"
+            mavenVersion = defaultProvidedVersion()
+            jdkHome = "%env.${jdk.jdkHome}%"
         }
     }
 
-    requirements {
-        equals("teamcity.agent.jvm.os.name", os)
-    }
 })
+
+fun build(
+    os: BuildOS,
+    jdk: JdkVersion
+) = JdkAndOsBuildType(os, jdk).apply {
+    id("Build_${os.caption}_${jdk.caption}".toExtId())
+    name = "Build (${os.caption}, ${jdk.caption})"
+
+    steps {
+        maven {
+            goals = "clean package"
+            mavenVersion = defaultProvidedVersion()
+            jdkHome = "%env.${jdk.jdkHome}%"
+        }
+    }
+
+    os(this)
+}
+
+enum class BuildOS(
+    val caption: String,
+    setup: Requirements.() -> Unit
+) : (JdkAndOsBuildType) -> Unit {
+
+    Linux("Linux", setup = {
+        matches(param = "teamcity.agent.jvm.os.name", value = ".*[Ll]inux.*")
+        exists(param = "docker")
+        matches(param = "system.agent.name", value = "^(default-.*|dotnet-linux(-clean)?(-admin)?-app-?\\d+-\\d+)$")
+    }),
+
+    Windows("Windows", setup = {
+        matches(param = "teamcity.agent.jvm.os.name", value = ".*[wW]indows.*")
+        matches(param = "system.agent.name", value = "^(default-.*|dotnet-windows(-clean)?(-admin)?-app-?\\d+-\\d+)$")
+    }),
+
+    Mac("Mac", setup = {
+        matches(param = "teamcity.agent.jvm.os.name", value = ".*OS.*X.*")
+        matches(param = "system.agent.name", value = "^(default-.*|dotnet-mac-unit-?\\d+-\\d+)$")
+    }),
+
+    ;
+
+    override fun invoke(buildType: JdkAndOsBuildType) {
+        buildType.requirements(requirements)
+    }
+
+//    val suffix = "_" + caption.toLowerCase()
+
+    val requirements: Requirements.() -> Unit = {
+        setup()
+    }
+}
+
+enum class JdkVersion(teamcityProperty : String) {
+    JDK_8(teamcityProperty = "env.JDK_18_x64"),
+    JDK_11(teamcityProperty = "env.JDK_11_x64");
+
+    val suffix = "_" + name.toLowerCase()
+    val caption = name.toLowerCase().replace("_", "")
+
+    val jdkHome = "%$teamcityProperty%"
+}
+
